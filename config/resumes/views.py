bf36f44c28783cb22024f37668317c8b6333d3a0
18Ishika <ishika.datascientist@gmail.com>
2026-01-12 16:20:29 +0530

    Build a serialiser

--- config/resumes/views.py
+++ config/resumes/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Resume
+from .serializers import ResumeSerializer
 
-# Create your views here.
+class ResumeUploadView(APIView):
+
+    def post(self, request):
+        serializer = ResumeSerializer(data=request.data)
+
+        if serializer.is_valid():
+            resume = serializer.save()
+
+            # 🚀 Celery task will be triggered here later
+            # process_resume.delay(resume.id)
+
+            return Response(
+                ResumeSerializer(resume).data,
+                status=status.HTTP_201_CREATED
+            )
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
