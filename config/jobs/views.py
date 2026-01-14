6015175316f532e5e4768708bab1735779fbab92
18Ishika <ishika.datascientist@gmail.com>
2026-01-14 20:19:57 +0530

    Tested jobs and resumes thru postman

--- config/jobs/views.py
+++ config/jobs/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Job
+from .serializers import JobSerializer
 
-# Create your views here.
+class JobCreateView(APIView):
+
+    def post(self, request):
+        serializer = JobSerializer(data=request.data)
+
+        if serializer.is_valid():
+            job = serializer.save()
+            return Response(
+                JobSerializer(job).data,
+                status=status.HTTP_201_CREATED
+            )
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
