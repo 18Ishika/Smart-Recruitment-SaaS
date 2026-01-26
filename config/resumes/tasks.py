171141f9291751f786de0f769a5fec61be05106d
18Ishika <ishika.datascientist@gmail.com>
2026-01-26 09:50:11 +0530

    Implmented celery and redis work

--- config/resumes/tasks.py
+++ config/resumes/tasks.py
@@ -3,19 +3,23 @@ from .models import Resume
 from resumes.parsers import process_and_score_resume
 from jobs.models import Job
 
+from celery import shared_task
+from .models import Resume
+from resumes.parsers import process_and_score_resume
+from jobs.models import Job
+
 @shared_task(bind=True)
 def process_resume_task(self, resume_id):
     resume = Resume.objects.get(id=resume_id)
-    job = resume.job
-
-    resume.status = "PROCESSING"
+    resume.status = "Processing"
     resume.save()
 
-    result = process_and_score_resume(resume, job.description)
-
-    resume.score = result["score"]
-    resume.parsed_text = result["text"]
-    resume.status = "DONE"
-    resume.save()
-
+    try:
+        result = process_and_score_resume(resume, resume.job.description)
+        resume.status = "Processed"
+        resume.save()
+    except Exception:
+        resume.status = "Failed"
+        resume.save()
+        raise
     return resume.id
